3fa2a420104c8c24efff161438c50a795209d9a5
jrchyang <yujrchyang@gmail.com>
2024-05-14 03:23:35 +0000

    import std map and set container

---
+++ common/unorder_map.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <unordered_map>
+#include "bluefs/namespace.h"
+
+namespace BLUEFS_NAMESPACE {
+
+using std::unordered_map;
+using std::unordered_multimap;
+
+}
---
+++ common/unorder_set.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <unordered_set>
+#include "bluefs/namespace.h"
+
+namespace BLUEFS_NAMESPACE {
+
+using std::unordered_set;
+
+}
