886dae7071106bd647a8f3eae362a9e8efce14ff
jrchyang <yujrchyang@gmail.com>
2024-05-14 02:58:18 +0000

    rename abort function name

--- common/assert.h
+++ common/assert.h
@@ -4,9 +4,9 @@
 
 namespace BLUEFS_NAMESPACE {
 
-extern void _abort(const char *file, const int line, const char *func);
+extern void __abort(const char *file, const int line, const char *func);
 
-#define bluefs_abort() _abort(__FILE__, __LINE__, __func__)
+#define bluefs_abort() __abort(__FILE__, __LINE__, __func__)
 #define bluefs_assert(expr)                                                   \
   do {                                                                        \
     if (expr)                                                                 \
